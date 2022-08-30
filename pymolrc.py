b2d94296fdb0b29f82aa9ed4ae9cac98103c513f
Qi Wang <wangqi1900@gmail.com>
2022-08-31 00:00:35 +0800

    Update and rename pymolcmds.py to pymolrc.py

--- pymolrc.py
+++ pymolrc.py
@@ -5,19 +5,34 @@
 # 3. Put this script in the same folder, otherwise change the path in .pml file
 # ---------------------------------------------------------------------------------
 
-from pymol import cmd, util
-from PIL import Image
-from glob import glob
 import binascii
 import shutil
 import os
+
+from pymol import cmd, util
+from PIL import Image
+from glob import glob
+
 
 AMINOACIDS ={'VAL':'V', 'ILE':'I', 'LEU':'L', 'GLU':'E', 'GLN':'Q', \
              'ASP':'D', 'ASN':'N', 'HIS':'H', 'TRP':'W', 'PHE':'F', \
              'TYR':'Y', 'ARG':'R', 'LYS':'K', 'SER':'S', 'THR':'T', \
              'MET':'M', 'ALA':'A', 'GLY':'G', 'PRO':'P', 'CYS':'C'}
 
-def presetplus(obj_name="all", chain_name="A", ligand_sele="organic", label_pocket=1, label_hbdist=0):
+
+#################### Custom Subroutines ####################
+def incr_gui_width():
+    width = int(cmd.get("internal_gui_width"))
+    if width < 1000:
+        cmd.set("internal_gui_width", width+5)
+
+def decr_gui_width():
+    width = int(cmd.get("internal_gui_width"))
+    if width > 100:
+        cmd.set("internal_gui_width", width-5)
+
+
+def show_cpi(obj_name="all", chain_name="A", ligand_sele="organic", label_pocket=1, label_hbdist=0):
     """
 DESCRIPTION
     
@@ -25,16 +40,16 @@ DESCRIPTION
     
 SYNTAX
     
-    presetplus [ obj_name [, chain_name [, ligand_sele [, label_pocket [, label_hbdist ]]]]]
+    show_cpi [ obj_name [, chain_name [, ligand_sele [, label_pocket [, label_hbdist ]]]]]
 
 EXAMPLES
 
-    presetplus
-    presetplus C
-    presetplus label_hbdist = 1
-    presetplus ligand_sele = sele  # After creating a selection named "sele" by clicking the ligand
-    presetplus ligand_sele = sele, label_pocket = 0
-    presetplus reset
+    show_cpi
+    show_cpi C
+    show_cpi label_hbdist = 1
+    show_cpi ligand_sele = sele  # After creating a selection named "sele" by clicking the ligand
+    show_cpi ligand_sele = sele, label_pocket = 0
+    show_cpi reset
     
 ARGUMENTS
         
@@ -119,69 +134,24 @@ NOTES
         cmd.set("label_size", 15, "hbonds")
     else:
         cmd.set("label_size", 0, "hbonds")
-    
     cmd.zoom("pocket")
 
-def makemovie(movie_name="pymolmovie", duration=15):
+
+def show_ppi(obj, c1='A', c2='B', cutoff=4.5, hb=1, name=""):
     """
 DESCRIPTION
     
-    Generate movies while rotating the system.
+    Generate customized protein-protein interaction view.
     
 SYNTAX
     
-    makemovie [movie_name [, duration]]
+    show_ppi object, [c1 [, c2 [, cutoff[, hb[, name]]]]]
 
 EXAMPLES
 
-    makemovie cdk2_view
-    makemovie cdk2_view, 30
-    
-ARGUMENTS
-
-    movie_name  = string: name of the movie {default: "pymolmovie"}
-
-    duration    = float: duration of the movie in seconds {default: 15}
-        
-NOTES
-    
-    Considering performance, this command will perform 60 rotations for 6 degree/rotation.
-    """
-
-    randstr = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
-    tempdir = "temp-%s"%randstr
-    os.mkdir(tempdir)
-    
-    cmd.mset("1 x60")
-    util.mroll(1,60,1)
-    cmd.mclear()
-    cmd.mpng("%s%s%s"%(tempdir, os.sep, movie_name))
-    
-    frames = []
-    for each_image in glob("%s%s%s*.png"%(tempdir, os.sep, movie_name)):
-        each_frame = Image.open(each_image)
-        frames.append(each_frame)
-
-    duration_per_frame_in_ms = float(duration)*1000 / 60
-    
-    frames[0].save(movie_name+".gif", append_images=frames[1:], save_all=True, duration=duration_per_frame_in_ms, loop=0)
-    shutil.rmtree(tempdir)
-
-def interface(obj, c1='A', c2='B', cutoff=4.5, hb=1, name=""):
-    """
-DESCRIPTION
-    
-    Generate customized protein-protein interface view.
-    
-SYNTAX
-    
-    interface object, [c1 [, c2 [, cutoff[, hb[, name]]]]]
-
-EXAMPLES
-
-    interface 5tbm
-    interface 5tbm, hb=0
-    interface 5tbm, hb=0, name=before
+    show_ppi 5tbm
+    show_ppi 5tbm, hb=0
+    show_ppi 5tbm, hb=0, name=before
 
 ARGUMENTS
 
@@ -228,20 +198,129 @@ NOTES
     cmd.delete("tempA")
     cmd.delete("tempB")
 
-def incr_gui_width():
-    width = int(cmd.get("internal_gui_width"))
-    if width < 1000:
-        cmd.set("internal_gui_width", width+5)
 
-def decr_gui_width():
-    width = int(cmd.get("internal_gui_width"))
-    if width > 100:
-        cmd.set("internal_gui_width", width-5)
+def show_abi(obj):
+    """
+DESCRIPTION
+    
+    Generate customized antibody-antigen interaction view. 
+    (Assume heavy and light chain names are H and L, respectively)
+    
+SYNTAX
+    
+    show_abi object
+
+EXAMPLES
+
+    show_abi 3hfm
+    
+ARGUMENTS
+
+    obj    = string: name of complex containing antigen and H & L chains of antibody.
+    """
+
+    # 1. Load complex and set color by chain
+    util.cbc()
+    cmd.create('complex', obj)
+    cmd.disable(obj)
+    cmd.cartoon('automatic', 'complex')
+
+    # 2. Create handle of antibody and antigen
+    cmd.select('ab', 'complex and (chain H+L)')
+    cmd.select('ag', 'complex and not ab')
+    cmd.select('interface', 'byres ((ab within 3 of ag) or (ag within 3 of ab))')
+    cmd.create('interface', 'interface')
+    cmd.create('ab', 'ab')
+    cmd.create('ag', 'ag')
+
+    # 3. Generate vaccum electrostatic potential surface
+    cmd.set('surface_carve_selection', 'interface')
+    cmd.set('surface_carve_cutoff', '3')
+    util.protein_vacuum_esp('ab')
+    util.protein_vacuum_esp('ag')
+
+    # 4. Setup aesthetics and save session
+    cmd.set('ray_opaque_background',1)
+    cmd.bg_color('white')
+    cmd.group('ab_grp', 'ab*')
+    cmd.group('ag_grp', 'ag*')
+    cmd.disable('interface or complex')
+    cmd.disable('*_map or *_pot')
+    cmd.show_as('sticks', 'interface')
+    cmd.label('interface and name CA', '"%s-%s" % (resn,resi)')
+    util.cnc('interface')
+    cmd.center()
+
+
+def make_movie(movie_name="pymol_movie", duration=15):
+    """
+DESCRIPTION
+    
+    Generate movies while rotating the system.
+    
+SYNTAX
+    
+    make_movie [movie_name [, duration]]
+
+EXAMPLES
+
+    make_movie cdk2_view
+    make_movie cdk2_view, 30
+    
+ARGUMENTS
+
+    movie_name  = string: name of the movie {default: "pymol_movie"}
+
+    duration    = float: duration of the movie in seconds {default: 15}
+        
+NOTES
+    
+    Considering performance, this command will perform 60 rotations for 6 degree/rotation.
+    """
+
+    randstr = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
+    tempdir = "temp-%s"%randstr
+    os.mkdir(tempdir)
+    
+    cmd.mset("1 x60")
+    util.mroll(1,60,1)
+    cmd.mclear()
+    cmd.mpng("%s%s%s"%(tempdir, os.sep, movie_name))
+    
+    frames = []
+    for each_image in glob("%s%s%s*.png"%(tempdir, os.sep, movie_name)):
+        each_frame = Image.open(each_image)
+        frames.append(each_frame)
+
+    duration_per_frame_in_ms = float(duration)*1000 / 60
+    
+    frames[0].save(movie_name+".gif", append_images=frames[1:], save_all=True, duration=duration_per_frame_in_ms, loop=0)
+    shutil.rmtree(tempdir)
+
+
+def ambient_occlusion_setup():
+    """
+    Ambient occulsion setup for fancy images rendering.
+    """
+    cmd.set('light_count', 8)
+    cmd.set('spec_count', 1)
+    cmd.set('shininess', 10)
+    cmd.set('specular', 0.25)
+    cmd.set('ambient', 0)
+    cmd.set('direct', 0)
+    cmd.set('reflect', 1.7)
+    cmd.set('ray_shadow_decay_factor', 0.1)
+    cmd.set('ray_shadow_decay_range', 2)
+    cmd.unset('depth_cue') 
+
 
 # COMMANDS 
-cmd.extend("presetplus", presetplus)
-cmd.extend("makemovie", makemovie)
-cmd.extend("interface", interface)
+cmd.extend("show_abi", show_abi)
+cmd.extend("show_cpi", show_cpi)
+cmd.extend("show_ppi", show_ppi)
+cmd.extend("make_movie", make_movie)
+cmd.extend("ambient_occlusion_setup", ambient_occlusion_setup)
+
 
 # HOTKEYS
 cmd.set_key( 'pgup' , incr_gui_width )
